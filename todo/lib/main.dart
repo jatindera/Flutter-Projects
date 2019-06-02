e942da77180d7f22a34f7c6a81383dcf9a22dda7
Dimple Arora <dimps.capri@gmail.com>
2019-06-02 15:45:50 +0530

    Flutter ToDO app

---
+++ todo/lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Todo());
+
+class Todo extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoList(title: 'ToDo List'),
+    );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  TodoList({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  List<String> _todoItems = [];
+
+  // This will be called each time the + button is pressed
+  void _addTodoItem(String task) {
+    // Putting our code inside "setState" tells the app that our state has changed, and
+    // it will automatically re-render the list
+    if (task.length > 0) {
+      setState(() {
+        _todoItems.add(task);
+      });
+    }
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+
+  // Show an alert dialog asking the user to confirm that the task is done
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+              title: new Text('Mark "${_todoItems[index]}" as done?'),
+              actions: <Widget>[
+                new FlatButton(
+                    child: new Text('CANCEL'),
+                    onPressed: () => Navigator.of(context).pop()),
+                new FlatButton(
+                    child: new Text('MARK AS DONE'),
+                    onPressed: () {
+                      _removeTodoItem(index);
+                      Navigator.of(context).pop();
+                    })
+              ]);
+        });
+  }
+
+  void _pushAddTodoScreen() {
+    //push this page on to the stack
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Add a new task'),
+        ),
+        body: new TextField(
+          autofocus: true,
+          onSubmitted: (val) {
+            _addTodoItem(val);
+            Navigator.pop(context);
+          },
+          decoration: new InputDecoration(
+              hintText: 'Enter a new to-do task',
+              contentPadding: const EdgeInsets.all(16.0)),
+        ),
+      );
+    }));
+  }
+
+  // Build the whole list of todo items
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        // itemBuilder will be automatically be called as many times as it takes for the
+        // list to fill up its available space, which is most likely more than the
+        // number of todo items we have. So, we need to check the index is OK.
+        if (index < _todoItems.length) {
+          return _buildRow(_todoItems[index], index);
+        }
+        return null;
+      },
+    );
+  }
+
+  // Build a single todo item
+  Widget _buildRow(String todoText, int index) {
+    return new ListTile(
+      title: new Text(todoText),
+      onTap: () => _promptRemoveTodoItem(index),
+      );
+  }
+
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Todo List')),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed:
+              _pushAddTodoScreen, // pressing this button now opens the new screen
+          tooltip: 'Add task',
+          child: new Icon(Icons.add)),
+    );
+  }
+}
